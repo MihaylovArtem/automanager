3be98d251f0f8fb1f9da6d3c4687ae758e793a48
Artem Mihajlov <MihaylovArtem74@gmail.com>
2016-04-15 21:21:43 +0500

    initial commit

---
+++ AutoManager/Classes/AMCategoriesVC/AMCategoryVC.h
@@ -0,0 +1,13 @@
+//
+//  AMCategoryVC.h
+//  AutoManager
+//
+//  Created by Artem Mihajlov on 11.04.16.
+//  Copyright © 2016 Program Ingeneering. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AMCategoryVC : UIViewController
+
+@end
